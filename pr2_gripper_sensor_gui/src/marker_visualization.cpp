e6016d8d9175bd2aa72bc75f446cf82c4f797695
Yannick Jonetzko <jonetzko@informatik.uni-hamburg.de>
2019-03-06 09:17:40 +0000

    adjust taxel positions after testing it on the robot

--- pr2_gripper_sensor_gui/src/marker_visualization.cpp
+++ pr2_gripper_sensor_gui/src/marker_visualization.cpp
@@ -8,11 +8,11 @@ ros::Publisher marker_publisher_;
 
 
 // Position and scale of the taxels in mm
-const float taxel_pos_x[22] = {27, 8.25, 21.5, 31, 31, 8.25, 21.5, 25, 25, 25, 19, 19, 19, 13, 13, 13, 7, 7, 7, 1, 1, 1};
+const float taxel_pos_x[22] = {27, 8.25, 21.5, 31, 31, 21.5, 8.25, 25, 25, 25, 19, 19, 19, 13, 13, 13, 7, 7, 7, 1, 1, 1};
 const float taxel_pos_y[22] = {-5, -10.5, -10.5, -10.5, -10.5, -10.5, -10.5, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8, -15.8};
 const float taxel_pos_z[22] = {0, 11.5, 11.5, 3, -3, -11.5, -11.5, 6, 0, -6, 6, 0, -6, 6, 0, -6, 6, 0, -6, 6, 0, -6};
 
-const float taxel_scale_x[22] = {3, 20, 5.5, 0.5, 0.5, 20, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
+const float taxel_scale_x[22] = {3, 20, 5.5, 0.5, 0.5, 5.5, 20, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
 const float taxel_scale_y[22] = {0.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5};
 const float taxel_scale_z[22] = {13, 0.5, 0.5, 5.5, 5.5, 0.5, 0.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5, 5.5};
 
@@ -54,7 +54,7 @@ void init() {
     marker.id = i + 22;
     marker.pose.position.x = taxel_pos_x[i] / 1000;
     marker.pose.position.y = taxel_pos_y[i] / -1000;
-    marker.pose.position.z = taxel_pos_z[i] / 1000;
+    marker.pose.position.z = taxel_pos_z[i] / -1000;
     marker.scale.x = taxel_scale_x[i] / 1000;
     marker.scale.y = taxel_scale_y[i] / 1000;
     marker.scale.z = taxel_scale_z[i] / 1000;
@@ -69,7 +69,7 @@ void callback(const pr2_gripper_sensor_msgs::PR2GripperSensorRawData::ConstPtr &
   float threshold_2 = 8000.0;
   float value;
 
-  for (int i=0; i<22; i++) {
+  for (int i=0; i<44; i++) {
     if (i<22)
       value = msg->left_finger_pad_forces[i];
     else
